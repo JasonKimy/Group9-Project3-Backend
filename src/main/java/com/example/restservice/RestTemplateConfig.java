26840c3af0568de644a7138a96eac26ecc205fb2
Micah <micahheneveld@gmail.com>
2025-12-08 14:50:52 -0800

    debugging, really just pushing to get heroku logs

--- src/main/java/com/example/restservice/RestTemplateConfig.java
+++ src/main/java/com/example/restservice/RestTemplateConfig.java
@@ -5,38 +5,30 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
 import java.net.HttpURLConnection;
 
 @Configuration
 public class RestTemplateConfig {
     
+    static {
+        // Enable PATCH method globally at class load time using reflection
+        try {
+            java.lang.reflect.Field methodsField = HttpURLConnection.class.getDeclaredField("methods");
+            methodsField.setAccessible(true);
+            
+            String[] existingMethods = (String[]) methodsField.get(null);
+            java.util.Set<String> methodSet = new java.util.LinkedHashSet<>(java.util.Arrays.asList(existingMethods));
+            methodSet.add("PATCH");
+            methodsField.set(null, methodSet.toArray(new String[0]));
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to enable PATCH method", e);
+        }
+    }
+    
     @Bean
     public RestTemplate restTemplate() {
-        // Create a custom request factory that enables PATCH method
-        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory() {
-            @Override
-            protected void prepareConnection(HttpURLConnection connection, String httpMethod) throws IOException {
-                super.prepareConnection(connection, httpMethod);
-                
-                // Enable PATCH method by using X-HTTP-Method-Override workaround if needed
-                if ("PATCH".equals(httpMethod)) {
-                    // Use reflection to allow PATCH method (workaround for Java 17 HttpURLConnection)
-                    try {
-                        java.lang.reflect.Field methodsField = HttpURLConnection.class.getDeclaredField("methods");
-                        methodsField.setAccessible(true);
-                        
-                        String[] methods = (String[]) methodsField.get(null);
-                        java.util.Set<String> methodSet = new java.util.LinkedHashSet<>(java.util.Arrays.asList(methods));
-                        methodSet.add("PATCH");
-                        methodsField.set(null, methodSet.toArray(new String[0]));
-                    } catch (Exception e) {
-                        throw new IOException("Failed to enable PATCH method", e);
-                    }
-                }
-            }
-        };
-        
+        // Use SimpleClientHttpRequestFactory with PATCH enabled via static block above
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
         return new RestTemplate(requestFactory);
     }
 }
